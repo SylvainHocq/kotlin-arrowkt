65eab397269206c2e856a8159edb93fe355b3d1e
Sylvain HOCQ <chtiboss@gmail.com>
2019-10-24 23:10:12 +0200

    Functor

--- src/main/kotlin/com/functional/programming/FunctorExo.kt
+++ src/main/kotlin/com/functional/programming/FunctorExo.kt
@@ -1,5 +1,13 @@
 package com.functional.programming
 
+import arrow.Kind
+import arrow.core.Option
+import arrow.core.Try
+import arrow.core.extensions.`try`.functor.functor
+import arrow.core.extensions.option.functor.functor
+import arrow.core.fix
+import arrow.typeclasses.Functor
+
 fun main() {
     val books = listOf(
         Book("ISBNTEST1", "Programming Kotlin"),
@@ -7,6 +15,12 @@ fun main() {
         Book("ISBNTEST3", "Functional Kotlin")
     )
 
-//TODO complete code ...
+    val result = books.filter { it.title.contains("465465", true) }.map(Book::title)
+    println("result :: $result")
+
+    fun <F> Functor<F>.addOne(fa: Kind<F, Int>) : Kind<F, Int> = fa.map { it + 1 }
+
+    Option.functor().addOne(Option(1)).fix()
+    Try.functor().addOne(Try { 1}).fix()
 
 }
